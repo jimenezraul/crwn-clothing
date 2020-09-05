b715dc34d97c9a08c32bff8adfbc96cdf2d651cc
jimenezraul <jimenezraul1981@gmail.com>
2020-09-04 21:38:12 -0400

    add sign in page

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './sign-in.styles.scss';
+
+class SignIn extends React.Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    render() {
+        return <div className='sign-in'>
+            <h2>already have an account</h2>
+            <span>Sign in with your email and password</span>
+
+            <form>
+                <input name='email' value={this.state.email} required />
+                <label>Email</label>
+                <input name='password' type="password" value={this.state.password} required />
+                <label>Password</label>
+
+                <input type="submit" />
+            </form>
+        </div>
+    }
+}
+
+export default SignIn;
