b4e913c9e7a064f7dbaf4425cb04dd150a6decc3
jimenezraul <jimenezraul1981@gmail.com>
2020-09-11 23:16:13 -0400

    created sign up component

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -6,7 +6,7 @@ import SignIn from '../../components/sign-in/sign-in.component';
 import SignUp from '../../components/sign-up/sign-up.component';
 
 const SignInAndSignUpPage = () => (
-    <div>
+    <div className='sign-in-and-sign-up'>
         <SignIn />
         <SignUp />
     </div>
